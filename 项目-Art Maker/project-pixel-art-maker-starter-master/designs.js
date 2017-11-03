d940417a59d145894f391034a1d107cb6354a69b
zhangyuwei <570146800@qq.com>
2017-11-03 14:24:08 +0800

    : feat(js): crate a project about Art Maker fix some problem about portfololi

---
+++ 项目-Art Maker/project-pixel-art-maker-starter-master/designs.js
@@ -0,0 +1,65 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+let tableColor;
+
+let isKeydown = false;
+
+function makeGrid() {
+    genertateTable();
+}
+
+makeGrid();
+
+function genertateTable() {
+    // get width and high
+    let tableLength = $('#input_height').val();
+    let tanleWidth = $('#input_width').val();
+
+    // create table
+    let table = $('#pixel_canvas');
+    table.html("");
+    for (let i = 0; i < tableLength; i++) {
+
+        let tr = $('<tr></tr>');
+        table.append(tr);
+
+        for (let j = 0; j < tanleWidth; j++) {
+            tr.append('<td></td>');
+        }
+    }
+}
+
+$(function(){
+   tableColor = $('#colorPicker').val();
+})
+
+// user tap submit
+$( '#submit' ).on( 'click', function() {
+    genertateTable();
+});
+
+$( '#pixel_canvas' ).on( 'mousemove', 'td', function(e) {
+
+    if (e.which === 1){
+        $(this).css('background-color', tableColor);
+    }
+
+});
+
+$( '#colorPicker' ).on( 'change', function() {
+    tableColor = $(this).val();
+});
+
+
+// $( '#pixel_canvas' ).on( 'keypress', function() {
+//     alert(isKeydown);
+//     if (isKeydown) {
+//         isKeydown = false;
+//     }else {
+//         isKeydown = ture;
+//     }
+// });
+
